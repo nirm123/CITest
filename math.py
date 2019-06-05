788678725b96b805444d7ba0197ee360805c4a6e
Prakash <Prakash.Nirmal@principal.com>
2019-06-05 10:21:53 -0500

    created default files

---
+++ math.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a/b
+
+if __name__ == "__main__":
+    print(add(1, 1))
+    print(subtract(1, 0))
+    print(multiply(2, 3))
+    print(divide(4, 2))
